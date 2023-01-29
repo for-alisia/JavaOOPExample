96a84516bf257879cd15a6a5762be219802492cc
Elena <for-alisia@yandex.ru>
2023-01-29 21:36:10 +0100

    Added setCoords methos to figure class

--- src/main/java/elenaromanova/chess/Figure.java
+++ src/main/java/elenaromanova/chess/Figure.java
@@ -2,7 +2,12 @@ package elenaromanova.chess;
 
 public abstract class Figure {
     Color color;
+    Coordinates coordinates;
     public Figure(Color color) {
         this.color = color;
+    }
+
+    public void setCoordinates(Coordinates coords) {
+        coordinates = coords;
     }
 }
