49c0575f0d9dc3de2de233df9201f2a45b372ab1
Elena <for-alisia@yandex.ru>
2023-04-03 16:50:11 +0200

    added record example

--- src/main/java/elenaromanova/bigdata/BigData.java
+++ src/main/java/elenaromanova/bigdata/BigData.java
@@ -3,17 +3,49 @@ package elenaromanova.bigdata;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.LongSummaryStatistics;
 import java.util.stream.Collectors;
 
 public class BigData {
-    public static void main(String[] args) {
+    record Person(String firstName, String lastName, long salary, String state) {}
+
+    public static String path = "/Users/alisia/Projects/JavaProjects/basicJava/examplesOOP/Hr5m.csv";
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
+        exampleWithRecord();
+        // exampleWithStrings();
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+    }
+
+    public static void exampleWithRecord() {
         try {
-            long recordsCount = Files
-                .lines(Path.of("/Users/alisia/Projects/JavaProjects/basicJava/examplesOOP/Hr5m.csv"))
+            long result = Files
+                .lines(Path.of(path))
+                .parallel() // to make stream faster - with this it takes approx 5 sec
                 .skip(1)
-                // .count(); // One option to count all items
-                .collect(Collectors.counting());
-            System.out.println(recordsCount);
+                .map(s -> s.split(","))
+                .map(a -> new Person(a[2], a[4], Long.parseLong(a[25]), a[32]))
+                .collect(Collectors.summingLong(Person::salary));
+
+            System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void exampleWithStrings() {
+        try {
+            long result = Files
+                    .lines(Path.of(path))
+                    .parallel() // to make stream faster - with this it takes approx 5 sec
+                    .skip(1)
+                    .map(s -> s.split(",")[25])
+                    // .collect(Collectors.summingLong(s -> Long.parseLong(s))); // this takes approx. 7 sec
+                    .mapToLong(Long::parseLong).sum(); // this also takes approx 7 sec
+            // .count(); // One option to count all items
+            // .collect(Collectors.counting()); // Second option to count amount of elements
+            System.out.println(result);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
