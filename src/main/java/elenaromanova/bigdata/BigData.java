9a40894bead60709fb84ba8a2c133fc549e10494
Elena <for-alisia@yandex.ru>
2023-04-03 17:10:01 +0200

    started grouping example

--- src/main/java/elenaromanova/bigdata/BigData.java
+++ src/main/java/elenaromanova/bigdata/BigData.java
@@ -3,7 +3,10 @@ package elenaromanova.bigdata;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.LongSummaryStatistics;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class BigData {
@@ -29,6 +32,20 @@ public class BigData {
                 .collect(Collectors.summingLong(Person::salary));
 
             System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void groupingExample() {
+        try {
+            Map<String, List<Person>> groupByState = Files
+                    .lines(Path.of(path))
+                    .parallel() // to make stream faster - with this it takes approx 5 sec
+                    .skip(1)
+                    .map(s -> s.split(","))
+                    .map(a -> new Person(a[2], a[4], Long.parseLong(a[25]), a[32]))
+                    .collect(Collectors.groupingBy(Person::state, TreeMap::new, Collectors.toList()));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
